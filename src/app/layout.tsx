a6238fdb401ecd70e9a272eb2132d3413e7fa8b0
Nhi Vu <vuvannhi@gmail.com>
2026-04-14 22:04:50 -0700

    Auto-commit: Update 1 file (src/app/layout.tsx,)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${poppins.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${poppins.variable} ${geistMono.variable} h-full antialiased dark`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
